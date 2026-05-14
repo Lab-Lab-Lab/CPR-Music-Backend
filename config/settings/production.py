4c5b8c0d993e341dda631057157099fac4fc1f1b
Michael Stewart <stewarmc@jmu.edu>
2026-05-14 14:42:07 -0400

    update storage settings for django 5.1

--- config/settings/production.py
+++ config/settings/production.py
@@ -105,12 +105,16 @@ AWS_S3_CUSTOM_DOMAIN = env("DJANGO_AWS_S3_CUSTOM_DOMAIN", default=None)
 aws_s3_domain = AWS_S3_CUSTOM_DOMAIN or f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 # STATIC
 # ------------------------
-STATICFILES_STORAGE = "teleband.utils.storages.StaticRootS3Boto3Storage"
+STORAGES = {
+    "default": {
+        "BACKEND": "teleband.utils.storages.MediaRootS3Boto3Storage",
+    },
+    "staticfiles": {
+        "BACKEND": "teleband.utils.storages.StaticRootS3Boto3Storage",
+    },
+}
 COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
 STATIC_URL = f"https://{aws_s3_domain}/static/"
-# MEDIA
-# ------------------------------------------------------------------------------
-DEFAULT_FILE_STORAGE = "teleband.utils.storages.MediaRootS3Boto3Storage"
 MEDIA_URL = f"https://{aws_s3_domain}/media/"
 
 # EMAIL
